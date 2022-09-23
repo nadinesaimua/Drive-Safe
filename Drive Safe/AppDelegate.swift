0c996f0776d7feb937737b4dae37c7cc3fde8c68
Nadine Saimua <78096232+nadinesaimua@users.noreply.github.com>
2022-09-23 16:02:39 +0300

    Update AppDelegate.swift

--- Drive Safe/AppDelegate.swift
+++ Drive Safe/AppDelegate.swift
@@ -26,4 +26,260 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
+    install! 'cocoapods', :warn_for_unused_master_specs_repo => false
+
+platform :ios, '11.0'
+use_frameworks!
+
+pod 'MapboxCoreNavigation', '~> 2.7.1'
+pod 'MapboxNavigation', '~> 2.7.1'
+
+target 'Navigation-Examples' do
+end
+
+target 'DocsCode' do
+end
 }
+@IBOutlet weak var beginNavigation: UIButton!
+    @IBOutlet weak var simulateNavigation: UISwitch!
+    
+    var exampleClass: UIViewController.Type?
+    var exampleName: String?
+    var exampleDescription: String?
+    var exampleStoryboard: UIStoryboard?
+    var hasEnteredExample = false
+    var pushExampleToViewController = false
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationItem.title = exampleName
+        
+        if exampleClass == nil {
+            beginNavigation.setTitle("Example Not Found", for: .normal)
+            beginNavigation.isEnabled = false
+            simulateNavigation.isEnabled = false
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if hasEnteredExample {
+            if let last = view.subviews.last {
+                last.removeFromSuperview()
+                hasEnteredExample = false
+            }
+        }
+    }
+    
+    @IBAction func didTapBeginNavigation(_ sender: Any) {
+        let controller = instantiate(example: exampleClass!, from: exampleStoryboard)
+        embed(controller: controller, shouldPush: pushExampleToViewController)
+    }
+    
+    private func instantiate<T: UIViewController>(example: T.Type, from storyboard: UIStoryboard? = nil) -> T {
+        return storyboard?.instantiateInitialViewController() as? T ?? example.init()
+    }
+    
+    private func embed(controller: UIViewController, shouldPush: Bool) {
+        addChild(controller)
+        view.addSubview(controller.view)
+
+        controller.didMove(toParent: self)
+        if shouldPush {
+            navigationController?.pushViewController(controller, animated: true)
+        }
+        hasEnteredExample = true
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForFooterInSection section: Int) -> String? {
+        guard let exampleDescription = exampleDescription else { return nil }
+        return section == tableView.numberOfSections - 1  ? exampleDescription : nil
+    }
+    
+    @IBAction func didToggleSimulateNavigation(_ sender: Any) {
+        simulationIsEnabled = simulateNavigation.isOn
+    }
+typealias NamedController = (
+    name: String,
+    description: String,
+    controller: UIViewController.Type,
+    storyboard: UIStoryboard?, // Is the example containined in a storyboard? If so, we assume the Initial View Controller of the storyboard.
+    pushExampleToViewController: Bool // If the example does not go directly into the example,(i.e. another map is shown) set this value to true
+)
+
+let listOfExamples: [NamedController] = [
+    (
+        name: "Advanced Implementation",
+        description:"""
+        Demonstrates how to display a custom map style and how to apply stylized components in the UI.
+        This example also allows the user to select an alternate route. Long press on the map to begin.
+        NavigationViewController reuses a NavigationMapView instance, allowing for a seamless transition
+        between between a route preview and active turn-by-turn navigation.
+        Note: The Directions API will not always return alternative routes.
+        """,
+        controller: AdvancedViewController.self,
+        storyboard: nil,
+        pushExampleToViewController: true
+    ),
+    (
+        name: "Basic",
+        description: "A basic hello world example showing how to create a navigation experience using the fewest lines of code possible.",
+        controller: BasicViewController.self,
+        storyboard: nil,
+        pushExampleToViewController: false
+    ),
+    (
+        name: "Custom Destination Marker",
+        description: "Use a custom image for styling the destination marker.",
+        controller: CustomDestinationMarkerController.self,
+        storyboard: nil,
+        pushExampleToViewController: true
+    ),
+    (
+        name: "Embedded View Controller",
+        description: "Demonstrates how to embed a NavigationViewController within a parent view controller.",
+        controller: EmbeddedExampleViewController.self,
+        storyboard: UIStoryboard(name: "EmbeddedExamples", bundle: nil),
+        pushExampleToViewController: true
+    ),
+    (
+        name: "Styled UI Elements",
+        description: "Demonstrates how to customize various UI elements and also change the map style.",
+        controller: CustomStyleUIElements.self,
+        storyboard: nil,
+        pushExampleToViewController: false
+    ),
+    (
+        name: "Waypoint Arrival Screen",
+        description: "Demonstrates how to provide a custom UIView for the user upon arriving at a waypoint.",
+        controller: WaypointArrivalScreenViewController.self,
+        storyboard: nil,
+        pushExampleToViewController: false
+    ),
+    (
+        name: "Directions API beta query parameters",
+        description: "Demonstrates how to subclass NavigationRouteOptions to take advantage of the beta query parameters available from the Directions API.",
+        controller: BetaQueryViewController.self,
+        storyboard: nil,
+        pushExampleToViewController: true
+    ),
+    (
+        name: "Custom Waypoint Styling",
+        description: "Demonstrates how to customize waypoint styling.",
+        controller: CustomWaypointsViewController.self,
+        storyboard: nil,
+        pushExampleToViewController: true
+    ),
+    (
+        name: "Custom Voice Controller",
+        description: "Add custom audio recordings for your instructions.",
+        controller: CustomVoiceControllerUI.self,
+        storyboard: nil,
+        pushExampleToViewController: false
+    ),
+    (
+        name: "Custom Directions Server",
+        description: "Use a custom directions server with the Navigation SDK via the Mapbox Map Matching SDK.",
+        controller: CustomServerViewController.self,
+        storyboard: nil,
+        pushExampleToViewController: false
+    ),
+    (
+        name: "Custom Top & Bottom Bars",
+        description: "Use a custom UI for top and bottom bars during navigation.",
+        controller: CustomBarsViewController.self,
+        storyboard: nil,
+        pushExampleToViewController: false
+    ),
+    (
+        name: "Route Deserialization",
+        description: "Demonstrates how to initialize a Route and deserialize it from JSON.",
+        controller: RouteDeserializationViewController.self,
+        storyboard: nil,
+        pushExampleToViewController: false
+    ),
+    (
+        name: "Route Initialization",
+        description: "Demonstrates how to initialize a Route and RouteResponse using initializers in code.",
+        controller: RouteInitializationViewController.self,
+        storyboard: nil,
+        pushExampleToViewController: false
+    ),
+    (
+        name: "Building Extrusion",
+        description: "Demonstrates how to highlight building extrusion.",
+        controller: BuildingExtrusionViewController.self,
+        storyboard: nil,
+        pushExampleToViewController: true
+    ),
+    (
+        name: "Predictive Caching",
+        description: "Demonstrates how to use predictive caching for navigation.",
+        controller: PredictiveCachingViewController.self,
+        storyboard: nil,
+        pushExampleToViewController: false
+    ),
+    (
+        name: "Route Alerts",
+        description: "Demonstrates how to display route alerts.",
+        controller: RouteAlertsViewController.self,
+        storyboard: nil,
+        pushExampleToViewController: false
+    ),
+    (
+        name: "Location Snapping",
+        description: "Demonstrates how to snap user location to the road network in a map view outside of active turn-by-turn navigation. Simulate Navigation option isn't supported here, instead you can use location simulation inside of the Simulator (Features ‣ Location ‣ \"City Bicycle Ride\") to see the difference with and without snapping.",
+        controller: LocationSnappingViewController.self,
+        storyboard: nil,
+        pushExampleToViewController: true
+    ),
+    (
+        name: "Electronic Horizon Events Observing",
+        description: "Demonstrates how to use electronic horizon to predict user's most probable path and show upcoming intersections. Simulate Navigation option isn't supported here, instead you can simulate location in Xcode.",
+        controller: ElectronicHorizonEventsViewController.self,
+        storyboard: nil,
+        pushExampleToViewController: true
+    ),
+    (
+        name: "Custom Navigation Camera",
+        description: "Demonstrates how to add custom data source and transitions to navigation camera.",
+        controller: CustomNavigationCameraViewController.self,
+        storyboard: nil,
+        pushExampleToViewController: true
+    ),
+    (
+        name: "Route Lines Styling",
+        description: "Demonstrates how to provide custom styling for the route lines.",
+        controller: RouteLinesStylingViewController.self,
+        storyboard: nil,
+        pushExampleToViewController: true
+    ),
+    (
+        name: "Custom Segue",
+        description: "Demonstrates how to create an instance of NavigationViewController from UIStoryboardSegue.",
+        controller: SegueViewController.self,
+        storyboard: UIStoryboard(name: "CustomSegue", bundle: nil),
+        pushExampleToViewController: true
+    ),
+    (
+        name: "Custom User Location",
+        description: "Demonstrates how to provide custom user location indicator layer during navigation.",
+        controller: CustomUserLocationViewController.self,
+        storyboard: nil,
+        pushExampleToViewController: true
+    ),
+    (
+        name: "Offline Regions",
+        description: "Demonstrates how to create a custom TileStore and handle offline regions.",
+        controller: OfflineRegionsViewController.self,
+        storyboard: nil,
+        pushExampleToViewController: true
+    ),
+    (
+        name: "Custom RoutingProvider",
+        description: "Demonstrates how to implement and utilize custom `RoutingProvider`.",
+        controller: CustomRoutingProviderViewController.self,
+        storyboard: nil,
+        pushExampleToViewController: true
+    )
+]
